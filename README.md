a10ed21b6cfb4a661a21b630cf44c6fe22c00a26
mori-ne <fold.it.fold.it@gmail.com>
2022-08-04 13:24:27 +0900

    0804

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 fukuryu_gakkai_wp_template
-
+Wordpress theme
 C:\Users\nakasone\Local Sites\fukuryunewwptheme\app\public\wp-content\themes\fukuryu_gakkai_wp_template
--- functions.php
+++ functions.php
@@ -1,1 +1,16 @@
+<?php
+
 //テーマstyle.cssの読み込み
+function twpp_enqueue_styles()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'twpp_enqueue_styles');
+
+// titleタグ出力
+add_theme_support('title-tag');
+
+/**
+ * WP-SCSS：ページをロードするたびにscssファイルを強制的にコンパイル.
+ */
+define( 'WP_SCSS_ALWAYS_RECOMPILE', true );
